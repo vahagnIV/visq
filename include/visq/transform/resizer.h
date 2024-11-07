852546b69e17eddd9f95e44be29c6ba8d1c626dc
vyeghikyan-st <vyeghikyan@servicetitan.com>
2024-11-07 16:05:35 +0400

    Added documentation to resizer

--- include/visq/transform/resizer.h
+++ include/visq/transform/resizer.h
@@ -5,26 +5,40 @@
 #include <visq/transform/sampler.h>
 
 namespace visq {
-
+/*!
+ * @brief Resizes image from Sampler
+ */
 template<typename T>
 class Resizer {
 public:
 
-  Image<T> Resize(const Sampler<T> * sampler, size_t new_width, size_t new_height) const {
+  /*!
+   * @Brief Resizes the image and returns the new one.
+   * @param sampler The sampler of original image.
+   * @param new_width The width of the resized image.
+   * @param new_height The height of the resized image. 
+   * @return A new instance of Image<T> with the provided width and height with resized data.
+   */
+  static Image<T> Resize(const Sampler<T> * const sampler, size_t new_width, size_t new_height) {
     Image<T> resizedImage(new_width, new_height, sampler->GetImage().GetChannels());
-    Resize(sampler, resizedImage);
+    Resize(sampler, &resizedImage);
     return resizedImage;
   }
 
-  void Resize(const Sampler<T> * sampler, Image<T> &image_to) const {
-    double scale_x = static_cast<double>(sampler->GetImage().GetWidth()) /  image_to.GetWidth();
-    double scale_y = static_cast<double>(sampler->GetImage().GetHeight()) /  image_to.GetHeight();
-    for(size_t y = 0; y < image_to.GetHeight(); ++y) {
+  /*!
+   * @brief Resizes the image into the provided one.
+   * @param sampler The sampler of original image.
+   * @param image_to The target image to resize image to.
+   */
+  static void Resize(const Sampler<T> * const sampler, Image<T> * image_to) {
+    double scale_x = static_cast<double>(sampler->GetImage().GetWidth()) /  image_to->GetWidth();
+    double scale_y = static_cast<double>(sampler->GetImage().GetHeight()) /  image_to->GetHeight();
+    for(size_t y = 0; y < image_to->GetHeight(); ++y) {
       double y_o = scale_y * y;
-      for(size_t x = 0; x < image_to.GetWidth(); ++x) {
+      for(size_t x = 0; x < image_to->GetWidth(); ++x) {
       double x_o = scale_x * x;
-        for(size_t c = 0; c < image_to.GetChannels(); ++c) {
-          image_to.Set(sampler->Interpolate(y_o, x_o, c), y,x,c);
+        for(size_t c = 0; c < image_to->GetChannels(); ++c) {
+          image_to->Set(sampler->Interpolate(y_o, x_o, c), y,x,c);
         }
       }
     }
